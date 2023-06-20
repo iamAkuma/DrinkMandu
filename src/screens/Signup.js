4c2a5ebbdfb4b6a6a3298a3b1b8b855e78ecbfa1
Saral Bajracharya <rmanbajra@gmail.com>
2023-06-20 20:08:08 +0545

    milya chaina milauna parcha

---
+++ src/screens/Signup.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Signup() {
+    const [credentials, setcredentials] = useState({ name: "", email: "", password: "", Geolocation: "" })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/createuser", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation })
+    
+        });
+        const json = await response.json()
+        console.log(json);
+
+        if (!json.success) {
+            alert("Enter Valid Credentials")
+
+        }
+    }
+    const onChange = (event) => {
+        setcredentials({ ...credentials, [event.target.name]: event.target.value })
+    }
+    return (
+        <>
+            <div className="container">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" placeholder="Enter name"name="name" value={credentials.name} onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" name="email" value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" name="password" value={credentials.password} onChange={onChange} id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Address</label>
+                        <input type="text" className="form-control" name="Geolocation" value={credentials.Geolocation} onChange={onChange} id="exampleInputPassword1" placeholder="Address" />
+                    </div>
+                    <button type="submit" className="m-3 btn btn-success">Submit</button>
+                    <Link to="/login" className='m-3 mx-1 btn btn-danger'>Already a User</Link>
+                </form>
+            </div>
+
+        </>
+    )
+}
